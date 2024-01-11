600fc01c5e72f7f25ca04ab0c4f92f2b1dc417c9
SanSanKon <kaba100@yandex.ru>
2024-01-11 21:53:11 +0700

    decision for task 7.1

---
+++ src/App.tsx
@@ -0,0 +1,99 @@
+import {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+type DateTimeProps = {
+    date: string;
+}
+
+function withPrettyDate<T extends DateTimeProps>(WrappedComponent: React.FC <T>) {
+    return function DateTimePretty(props: T) {
+        const { date } = props;
+
+        const formatDate = () => {
+            const currentDate = new Date();
+            const videoDate = new Date(date);
+            const timeDifference = (currentDate.getTime() - videoDate.getTime()) / 1000;
+
+            if (timeDifference < 3600) {
+                const minutes = Math.floor(timeDifference / 60);
+                return `${minutes} минут назад`;
+            } else if (timeDifference < 86400) {
+                const hours = Math.floor(timeDifference / 3600);
+                return `${hours} часов назад`;
+            } else {
+                const days = Math.floor(timeDifference / 86400);
+                return `${days} дней назад`;
+            }
+        };
+
+        return (
+            <WrappedComponent {...props} date={formatDate()} />
+        );
+    };
+}
+
+function DateTime(props: DateTimeProps) {
+    return (
+        <p className="date">{props.date}</p>
+    )
+}
+
+const DateTimePretty = withPrettyDate(DateTime);
+
+interface VideoProps {
+    url: string;
+    date: string;
+}
+
+function Video(props: VideoProps) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            <DateTimePretty date={props.date} />
+        </div>
+    )
+}
+
+interface VideoListProps {
+    list: VideoProps[];
+}
+
+function VideoList(props: VideoListProps) {
+    return props.list.map(item => <Video key={uuidv4()} url={item.url} date={item.date} />);
+}
+
+function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-07-31 13:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-03-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-02-03 23:16:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-01 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-12-02 05:24:00'
+        },
+    ]);
+
+    return (
+        <VideoList list={list} />
+        //<DateTimePretty list={list} />
+    );
+}
+
+export default App
